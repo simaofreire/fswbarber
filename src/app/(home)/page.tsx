fd4ff472196c845661b7b061e77b24d98b5cddf8
Simão Freire <simaofreire@live.com>
2024-02-02 15:35:00 -0300

    feat: Add search component to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import Header from '../_components/header';
+import Search from './_components/search';
 
 export default function Home() {
 	const date = format(new Date(), "EEEE',' dd 'de' MMMM", { locale: ptBR });
@@ -12,6 +13,10 @@ export default function Home() {
 			<div className="px-5 pt-5">
 				<h2 className="text-xl font-bold">Olá Simão</h2>
 				<p className="capitalize text-sm">{date}</p>
+			</div>
+
+			<div className="px-5 mt-6">
+				<Search />
 			</div>
 		</div>
 	);
