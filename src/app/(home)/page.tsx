8b13f10589e77a2f10a5721c5cf565965fcae6cb
Simão Freire <simaofreire@live.com>
2024-02-02 15:28:34 -0300

    fea: Add date-fns library and display formatted date on home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,18 @@
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 import Header from '../_components/header';
 
 export default function Home() {
+	const date = format(new Date(), "EEEE',' dd 'de' MMMM", { locale: ptBR });
+
 	return (
 		<div>
 			<Header />
+
+			<div className="px-5 pt-5">
+				<h2 className="text-xl font-bold">Olá Simão</h2>
+				<p className="capitalize text-sm">{date}</p>
+			</div>
 		</div>
 	);
 }
